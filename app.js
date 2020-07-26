73a288112425e3a3d350a9fd3c84eb38f671a3b4
Noyon <denary.noyon@gmail.com>
2020-07-27 00:23:02 +0600

    remove suggestions category from search result

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ autocomplete('#aa-search-input', {}, [{
     source: autocomplete.sources.hits(bloop, { hitsPerPage: 7 }),
     displayKey: 'page_name',
     templates: {
-        header: '<div class="aa-suggestions-category">Facebook Pages</div>',
+        // header: '<div class="aa-suggestions-category">Facebook Pages</div>',
         suggestion({ _highlightResult }) {
             return `<span>${_highlightResult.page_name.value}</span>`;
         }
